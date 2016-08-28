53ddda5c4b74381312fd7df1674b32b8709f0ab5
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-29 00:00:12 +0200

    refactoring

--- bin/yamlformat/yamlformat.go
+++ bin/yamlformat/yamlformat.go
@@ -10,81 +10,73 @@ import (
 	"runtime"
 
 	io_util "github.com/bborbe/io/util"
-	"github.com/bborbe/log"
+	"github.com/golang/glog"
 	"gopkg.in/yaml.v2"
 )
 
 const (
-	PARAMETER_LOGLEVEL = "loglevel"
-	PARAMETER_PATH     = "path"
-	PARAMETER_WRITE    = "write"
+	PARAMETER_PATH  = "path"
+	PARAMETER_WRITE = "write"
 )
 
 var (
-	logger      = log.DefaultLogger
-	logLevelPtr = flag.String(PARAMETER_LOGLEVEL, log.LogLevelToString(log.ERROR), log.FLAG_USAGE)
-	pathPtr     = flag.String(PARAMETER_PATH, "", "path")
-	writePtr    = flag.Bool(PARAMETER_WRITE, false, "write")
+	pathPtr  = flag.String(PARAMETER_PATH, "", "path")
+	writePtr = flag.Bool(PARAMETER_WRITE, false, "write")
 )
 
 func main() {
-	defer logger.Close()
+	defer glog.Flush()
+	glog.CopyStandardLogTo("info")
 	flag.Parse()
-
-	logger.SetLevelThreshold(log.LogStringToLevel(*logLevelPtr))
-	logger.Debugf("set log level to %s", *logLevelPtr)
-
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	writer := os.Stdout
 
 	err := do(writer, *pathPtr, *writePtr)
 	if err != nil {
-		logger.Fatal(err)
-		logger.Close()
-		os.Exit(1)
+		glog.Exit(err)
 	}
-	logger.Debug("done")
+	glog.V(2).Info("done")
 }
 
 func do(writer io.Writer, path string, write bool) error {
 	var err error
-	logger.Debugf("format %s and write %v", path, write)
+	glog.V(2).Infof("format %s and write %v", path, write)
 	if len(path) == 0 {
 		fmt.Fprintf(writer, "parameter %s missing\n", PARAMETER_PATH)
 		return nil
 	}
 	if path, err = io_util.NormalizePath(path); err != nil {
-		logger.Warnf("normalize path: %s failed: %v", path, err)
+		glog.Warningf("normalize path: %s failed: %v", path, err)
 		return err
 	}
 	source, err := ioutil.ReadFile(path)
 	if err != nil {
-		logger.Warnf("read file %s failed: %v", path, err)
+		glog.Warningf("read file %s failed: %v", path, err)
 		return err
 	}
 	var data interface{}
 	err = yaml.Unmarshal(source, &data)
 	if err != nil {
-		logger.Warnf("unmarshal %s failed: %v", path, err)
+		glog.Warningf("unmarshal %s failed: %v", path, err)
 		return err
 	}
 	content, err := yaml.Marshal(data)
 	if err != nil {
-		logger.Warnf("marshal failed: %v", err)
+		glog.Warningf("marshal failed: %v", err)
 		return err
 	}
 	if write {
-		logger.Debug("write file")
+		glog.V(2).Info("write file")
 		fileInfo, err := readMode(path)
 		if err != nil {
-			logger.Warnf("get fileinfo failed: %v", err)
+			glog.Warningf("get fileinfo failed: %v", err)
 			return err
 		}
-		logger.Debugf("write yaml %s", path)
+		glog.V(2).Infof("write yaml %s", path)
 		return ioutil.WriteFile(path, content, fileInfo.Mode())
 	} else {
-		logger.Debugf("print content")
+		glog.V(2).Infof("print content")
 		fmt.Fprintf(writer, "%s\n", string(content))
 		return nil
 	}
